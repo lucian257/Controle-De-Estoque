73c237f2f9d59d7d4f8b1db58035b076e56b2704
gsouzao <gustavoas034@gmail.com>
2020-05-28 16:53:12 -0300

    estilizando botao e pesquisa

--- index.php
+++ index.php
@@ -1,5 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Ínicio</title>
+	<link rel="stylesheet" href="assets/css/style.css">
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/bootstrap-grid.min.css">
+</head>
+<body>
+	<div class="container">
+
+		<div class= "conteudo">
+		
 <?php  
 
-	echo "<input type='text' placeholder='Ex: UN43NU7100AG'><input type='submit' value='Procurar'>";
+echo "<input type='text' placeholder='Ex: UN43NU7100AG' class='pesquisar'>
+<input type='submit' value='Procurar' class = 'botao' >";
 
 ?>
+</div>
+</div>
+</body>
+</html>
